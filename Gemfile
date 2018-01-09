b8477395effa451d0e2fc35a67f5e1c17b223de3
Bowen Sun <bowensun@gmail.com>
2018-01-09 17:44:49 +0000

    aws es change

--- Gemfile
+++ Gemfile
@@ -51,6 +51,7 @@ gem 'whenever', require: false
 gem 'will_paginate', '~> 3.0', '>=3.0.3'
 gem 'will_paginate-bootstrap', '~> 1.0', '>= 1.0.1'
 gem 'zendesk_api'
+gem 'faraday_middleware'
 
 gem 'carrierwave',
   git: 'https://github.com/carrierwaveuploader/carrierwave.git',
--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,4 +1,14 @@
+require 'faraday_middleware/aws_signers_v4'
+
 if Rails.env.production?
   Elasticsearch::Model.client =
-    Elasticsearch::Client.new(url: Rails.configuration.elastic_search_url)
+    Elasticsearch::Client.new(url: Rails.configuration.elastic_search_url) do |f|
+      f.request :aws_signers_v4,
+        credentials: Aws::Credentials.new(Rails.config.aws_elastic_key, Rails.config.aws_elastic_secret),
+        service_name: 'es',
+        region: Rails.config.aws_elastic_region
+
+      f.response :logger
+      f.adapter  Faraday.default_adapter
+    end
 end
