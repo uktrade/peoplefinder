7b17bef7ec82e1d2790ee506f682a97186c79f46
Bowen Sun <bowensun@gmail.com>
2018-01-09 18:11:06 +0000

    fix typo

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -4,9 +4,9 @@ if Rails.env.production?
   Elasticsearch::Model.client =
     Elasticsearch::Client.new(url: Rails.configuration.elastic_search_url) do |f|
       f.request :aws_signers_v4,
-        credentials: Aws::Credentials.new(Rails.config.aws_elastic_key, Rails.config.aws_elastic_secret),
+        credentials: Aws::Credentials.new(Rails.configuration.aws_elastic_key, Rails.configuration.aws_elastic_secret),
         service_name: 'es',
-        region: Rails.config.aws_elastic_region
+        region: Rails.configuration.aws_elastic_region
 
       f.response :logger
       f.adapter  Faraday.default_adapter
