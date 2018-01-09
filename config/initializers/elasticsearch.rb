e452ae3b102c0a0d8aac3f599ab04bf2cece88f0
Bowen Sun <bowensun@gmail.com>
2018-01-09 18:54:42 +0000

    fix ES transport

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,15 +1,14 @@
 require 'faraday_middleware/aws_signers_v4'
 
 if Rails.env.production?
-  transport_configuration = lambda do |f|
-    f.request :aws_signers_v4,
+  Elasticsearch::Model.client = Elasticsearch::Client.new(url: Rails.configuration.elastic_search_url) do |faraday|
+    faraday.request :aws_signers_v4,
       credentials: Aws::Credentials.new(Rails.configuration.aws_elastic_key, Rails.configuration.aws_elastic_secret),
       service_name: 'es',
       region: Rails.configuration.aws_elastic_region
 
-    f.response :logger
-    f.adapter  Faraday.default_adapter
+    faraday.response :json, :content_type => /\bjson\b/
+    faraday.response :raise_error
+    faraday.adapter Faraday.default_adapter
   end
-  transport = Elasticsearch::Transport::Transport::HTTP::Faraday.new url: Rails.configuration.elastic_search_url, &transport_configuration
-  Elasticsearch::Model.client = Elasticsearch::Client.new transport: transport
 end
