b3337d6577c2bf6c7baa39c2d038f1a9d05c1439
Stuart Axon <stu.axon@gmail.com>
2018-05-30 16:23:08 +0100

    Update codeclimate to simplecov Update factorygirl to factorybot Update postgres to 0.20 Update everything else

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,8 @@
 require File.expand_path('../boot', __FILE__)
 
+require 'capybara'
+require 'capybara/dsl'
+require 'capybara/rspec'
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
