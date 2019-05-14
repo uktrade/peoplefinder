cef7131e43da6d16ea4048de2449d6a439f8c636
Christian Sutter <git@doublesignal.com>
2019-05-14 14:23:25 +0000

    Remove legacy IE compatibility; bump frameworks
    This is the very first baby step towards migrating from the legacy GDS
    Frontend frameworks towards the new GOV.UK Design System.
    
    - Remove compatibility CSS for legacy IE versions (IE7-9)
    - Initial version bumps for govuk_* FE frameworks (without breaking
      changes)

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,28 +1,10 @@
 # frozen_string_literal: true
 
-# Do the gov-static, moj-base even exist?? docker output indicates not
 Rails.application.config.assets.precompile += %w[
   email.css
   Jcrop/css/jquery.Jcrop.min.css
   Jcrop/js/jquery.Jcrop.min.js
-  application-ie6.css
-  application-ie7.css
-  application-ie8.css
 ]
-# TODO: these were in the precompile array
-#       but look to be a leftover from
-#       old templates used. Remove once
-#       confident not required (they are
-#       not being compiled in any event)
-# gov-static/gov-goodbrowsers.css
-# gov-static/gov-ie6.css
-# gov-static/gov-ie7.css
-# gov-static/gov-ie8.css
-# gov-static/gov-fonts.css
-# gov-static/gov-fonts-ie8.css
-# gov-static/gov-print.css
-# moj-base.css
-# gov-static/gov-ie.js
 
 unless Rails.env.production?
   Rails.application.config.assets.precompile += %w[ teaspoon.css
