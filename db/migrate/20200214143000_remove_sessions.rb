690e04e609aced4dfc38ecf3fe6a6749c366e425
Christian Sutter <git@doublesignal.com>
2020-02-13 15:58:58 +0000

    Remove sessions table from database
    Not needed anymore now that we are successfully using Redis for session
    storage.

---
+++ db/migrate/20200214143000_remove_sessions.rb
@@ -0,0 +1,12 @@
+class RemoveSessions < ActiveRecord::Migration[6.0]
+  def change
+    drop_table :sessions do |t|
+      t.string "session_id", null: false
+      t.text "data"
+      t.datetime "created_at"
+      t.datetime "updated_at"
+      t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
+      t.index ["updated_at"], name: "index_sessions_on_updated_at"
+    end
+  end
+end
