fd12b3574a4d6c4e18377aa86e8a1cb81f2d08c8
Christian Sutter <git@doublesignal.com>
2020-02-14 17:50:19 +0000

    Downgrade Sidekiq until GOV.UK PaaS supports Redis 4
    The latest version of Sidekiq requires Redis 4.0 or higher – PaaS is
    stuck on 3.x.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,7 +2,9 @@
 
 Sidekiq.configure_server do |config|
   config.redis = { url: Rails.configuration.redis_sidekiq_url }
-  config.log_formatter = Sidekiq::Logger::Formatters::JSON.new
+
+  # TODO: Enable once we can upgrade to Sidekiq 6.0
+  # config.log_formatter = Sidekiq::Logger::Formatters::JSON.new
 end
 
 Sidekiq.configure_client do |config|
