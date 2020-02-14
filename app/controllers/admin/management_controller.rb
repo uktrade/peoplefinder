9ffbb0f05ce665c16b584e824e0d5064d0cb22bc
Christian Sutter <git@doublesignal.com>
2020-02-14 14:33:56 +0000

    Sidekiq tweaks (#273)
    - Log Sidekiq logs in JSON
    - Add basic stats to management page

--- app/controllers/admin/management_controller.rb
+++ app/controllers/admin/management_controller.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
+require 'sidekiq/api'
+
 module Admin
   class ManagementController < ApplicationController
     before_action :authorize_user
 
-    def show; end
+    def show
+      @sidekiq_stats = Sidekiq::Stats.new
+    end
 
     private
 
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,6 +2,7 @@
 
 Sidekiq.configure_server do |config|
   config.redis = { url: Rails.configuration.redis_sidekiq_url }
+  config.log_formatter = Sidekiq::Logger::Formatters::JSON.new
 end
 
 Sidekiq.configure_client do |config|
