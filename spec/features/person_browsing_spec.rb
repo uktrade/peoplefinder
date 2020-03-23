ef4b7e613002c7d5a91250e6ee8b0e10a9f3bd18
Christian Sutter <git@doublesignal.com>
2020-03-23 15:27:20 +0000

    Add basic value object and model method; refactoring (#306)
    - Add initial value object code
    - Reorder Person model macros into sane order
    - Inline or remove unnecessary model concerns
    - Update npm packages

--- spec/features/person_browsing_spec.rb
+++ spec/features/person_browsing_spec.rb
@@ -29,27 +29,6 @@ describe 'Person browsing' do
     expect(page).to have_selector('.breadcrumbs ol li', text: person.name)
   end
 
-  describe 'Days worked' do
-    let(:weekday_person) { create(:person, works_monday: true, works_friday: true) }
-    let(:weekend_person) { create(:person, works_saturday: true, works_sunday: true) }
-
-    it 'A person who only works weekdays should not see Saturday & Sunday listed' do
-      visit person_path(weekday_person)
-      expect(page).to have_xpath("//li[@alt='Monday']")
-      expect(page).to have_xpath("//li[@alt='Friday']")
-
-      expect(page).not_to have_xpath("//li[@alt='Sunday']")
-      expect(page).not_to have_xpath("//li[@alt='Saturday']")
-    end
-
-    it 'A person who works one or more days on a weekend should have their days listed' do
-      visit person_path(weekend_person)
-
-      expect(page).to have_xpath("//li[@alt='Sunday']")
-      expect(page).to have_xpath("//li[@alt='Saturday']")
-    end
-  end
-
   def create_group_hierarchy(*names)
     group = create(:department)
     names.each do |name|
