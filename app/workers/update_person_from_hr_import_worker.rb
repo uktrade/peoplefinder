ddc6fb44975a83a23f94c71052577424cc8c61d3
Christian Sutter <git@doublesignal.com>
2020-09-28 12:05:38 +0100

    WIP DO NOT MERGE - logging

--- app/workers/update_person_from_hr_import_worker.rb
+++ app/workers/update_person_from_hr_import_worker.rb
@@ -16,12 +16,15 @@ class UpdatePersonFromHrImportWorker
 
     unless person
       Rails.logger.warn("[hr-data-import] Could not match record '#{record.email}' to a People Finder user")
+      Rails.logger.info("[DATA] #{hr_data_row.join(',')},does not exist,")
       return
     end
 
     previous_grade = person.grade.to_s
+    previous_grade_old = HrDataRow::GRADE_MAPPINGS.key(person.grade&.to_sym) || person.grade&.humanize
     if previous_grade == record.mapped_grade.to_s
       Rails.logger.info("[hr-data-import] Grade for user '#{record.email}' has not changed")
+      Rails.logger.info("[DATA] #{hr_data_row.join(',')},grade matches already,#{previous_grade_old}")
       return
     end
 
@@ -30,10 +33,12 @@ class UpdatePersonFromHrImportWorker
     end
 
     if previous_grade.present?
+      Rails.logger.info("[DATA] #{hr_data_row.join(',')},grade would change,#{previous_grade_old}")
       Rails.logger.info(
         "[hr-data-import] Changed grade for user '#{record.email}' to #{record.mapped_grade} (was #{previous_grade})"
       )
     else
+      Rails.logger.info("[DATA] #{hr_data_row.join(',')},grade would be set,")
       Rails.logger.info("[hr-data-import] Set grade for user '#{record.email}' to #{record.mapped_grade}")
     end
   end
